ba0b0be9c0ba3c1c9efb59f20a3bfc6b999161a9
Mario Iván Martínez <mim@fcfm.buap.mx>
2013-06-22 16:22:05 -0500

    ver 3.0
    Ahora incluye la captura de fotos (2)

---
+++ js/API/captura.js
@@ -0,0 +1,13 @@
+// captura
+
+function tomarFoto() {
+	navigator.device.capture.captureImage(function(capObjects){
+		var cant = capObjects.length;
+		for (i=0; i<cant; i++)
+			foto = capObjects[i].fullPath;
+		$('#regFoto').attr('rel', foto);
+		$('#mFoto').html('<img src="'+foto+'" width="100%" />');
+	}, function(err){
+		navigator.notification.alert("Error: "+err.code, null, "Captura", "Aceptar");
+	}, {limit:2});
+}
--- js/API/eventos.js
+++ js/API/eventos.js
@@ -1,18 +1,24 @@
 // Eventos
 $(document).ready(function(e) {
-	if(!isLogin())
-		window.location.href = '#login';
-   $('#regSend').tap(function() {
-		if($('#regNom').val()!='' && $('#regTel').val()!='' && $('#regMail').val()!='') {
-			var nom = $('#regNom').val();
-			var tel = $('#regTel').val();
-			var mail = $('#regMail').val();
-			alert(nom + '\n' + tel + '\n' + mail);
-		}
-		else {
-			alert('Todos los campos son requeridos');
-		}
-	});
+	document.addEventListener("deviceready", function(){
+		if(!isLogin())
+			window.location.href = '#login';
+		$('#regSend').tap(function() {
+			if($('#regNom').val()!='' && $('#regTel').val()!='' && $('#regMail').val()!='' && $('#regFoto').attr('rel')!=undefined) {
+				var nom = $('#regNom').val();
+				var tel = $('#regTel').val();
+				var mail = $('#regMail').val();
+				var foto = $('#regFoto').attr('rel');
+				navigator.notification.alert(nom + '\n' + tel + '\n' + mail, null, "Hotel", "Aceptar");
+			}
+			else {
+				navigator.notification.alert('Todos los campos son requeridos', null, "Hotel", "Ok");
+			}
+		});
+		$('#regFoto').tap(function() {
+			tomarFoto();
+		});
+	}, false);
 });
 
 function isLogin() {
