0753122a10c4e19f560d4b929f91766161894964
Mario Iván Martínez <mim@fcfm.buap.mx>
2013-06-22 18:55:31 -0500

    ver 3.1.1
    Bug fix:
    ajax alert missing

--- js/API/servidor.js
+++ js/API/servidor.js
@@ -6,11 +6,12 @@ function enviarDatos(nom, tel, email, foto) {
 		url: "http://www.igitsoft.com/pgtest.php",
 		data: "nom="+nom+"&tel="+tel+"&mai="+email
 	}).done(function(msg) {
-		if(msg==1) {
-			subirFoto(foto);
-		}
-		else {
-			navigator.notification.alert("Error al Registrarse", null, "Registro", "Aceptar");
-		}
+			if(msg==1) {
+				subirFoto(foto);
+			}
+			else {
+				navigator.notification.alert("Error al Registrarse", null, "Registro", "Aceptar");
+			}
+			alert("Data Saved: "+msg);
 	});
 }
